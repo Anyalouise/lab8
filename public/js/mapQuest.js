accb40981a6afb0e25c222f7cf2f364129b7ba2a
Anya Bouzida <anyalouise@gmail.com>
2021-02-22 16:00:11 -0800

    added map, photo, and rsvp post

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,15 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'kf6c3FoRS0GFJGMQx2PYpfZvso085FgE';
+
+	// 'mao' refers to a ,div. element with the ID map
+	var map = L.mapquest.map('map', {
+		center: [32.8785, -117.2359],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	//Add a marker to the map
+	L.marker([32.88, -117.236]).addTo(map);
 }
